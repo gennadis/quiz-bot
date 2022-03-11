a23dc967d206cdd6cec2e9baa87daea4f1fa2629
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-11 21:43:18 +0300

    Refactor bot with new quiz methods

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,7 +15,7 @@ from telegram.ext import (
     RegexHandler,
 )
 
-from questions import collect_quiz_pairs, REDIS_CONN
+from questions import get_random_quiz, get_quiz_answer, REDIS_CONN, QUIZ_FILEPATH
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -39,9 +39,7 @@ def start(bot, update):
 
 
 def handle_new_question_request(bot, update):
-    quiz_pairs = collect_quiz_pairs("quiz-questions/1vs1200.txt")
-    question, answer = random.choice(list(quiz_pairs.items()))
-
+    question, answer = get_random_quiz(QUIZ_FILEPATH)
     REDIS_CONN.set(name=update.message.chat_id, value=question)
     update.message.reply_text(question)
 
@@ -50,9 +48,7 @@ def handle_new_question_request(bot, update):
 
 def handle_solution_attempt(bot, update):
     question = REDIS_CONN.get(name=update.message.chat_id)
-    quiz_pairs = collect_quiz_pairs("quiz-questions/1vs1200.txt")
-    full_answer = quiz_pairs[question.decode("UTF-8")]
-    answer, explanation = full_answer.split(".", maxsplit=1)
+    answer = get_quiz_answer(QUIZ_FILEPATH, question.decode("UTF-8"))
 
     if update.message.text.lower() == answer.lower():
         update.message.reply_text(
@@ -66,9 +62,7 @@ def handle_solution_attempt(bot, update):
 
 def handle_surrender(bot, update):
     question = REDIS_CONN.get(name=update.message.chat_id)
-    quiz_pairs = collect_quiz_pairs("quiz-questions/1vs1200.txt")
-    full_answer = quiz_pairs[question.decode("UTF-8")]
-    answer, explanation = full_answer.split(".", maxsplit=1)
+    answer = get_quiz_answer(QUIZ_FILEPATH, question.decode("UTF-8"))
 
     update.message.reply_text(answer)
 
