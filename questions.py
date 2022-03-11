dd27f9cadbf0c46b07ab8280e318909bd0bab0f2
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-11 20:37:06 +0300

    Refactor quiz items parsing

--- questions.py
+++ questions.py
@@ -10,22 +10,22 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 REDIS_CONN = redis.Redis(host=DB_URL, port=DB_PORT, db=DB_NAME, password=DB_PASSWORD)
 
 
-def collect_quiz_pairs(filename: str) -> dict:
+def parse_quiz_file(filename: str) -> dict:
     with open(filename, "r", encoding="KOI8-R") as file:
         text = file.read()
 
-    quiz_pairs = dict()
+    parsed_quiz_items = dict()
     questions, answers = [], []
 
     for line in text.split("\n\n"):
         if line.strip().startswith("Вопрос"):
             _, question_text = line.split(sep=":", maxsplit=1)
-            questions.append(question_text.strip())
+            questions.append(question_text.replace("\n", " ").strip())
         elif line.strip().startswith("Ответ"):
             _, answer_text = line.split(sep=":", maxsplit=1)
-            answers.append(answer_text.strip())
+            answers.append(answer_text.replace("\n", " ").strip())
 
     for question, answer in zip(questions, answers):
-        quiz_pairs[question] = answer
+        parsed_quiz_items[question] = answer
 
-    return quiz_pairs
+    return parsed_quiz_items
