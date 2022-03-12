9d40f60bb683883d1e587cf6e601ca021029c04b
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-12 20:27:25 +0300

    Add keyboard buttons

--- vk_bot.py
+++ vk_bot.py
@@ -1,25 +1,36 @@
 import os
 import random
 
-import vk_api as vk
+import vk_api
 from dotenv import load_dotenv
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id, message=event.text, random_id=random.randint(1, 1000)
-    )
+def main(vk_token: str):
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button("Новый вопрос", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Сдаться", color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button("Мой счет", color=VkKeyboardColor.POSITIVE)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            vk.messages.send(
+                peer_id=event.user_id,
+                random_id=get_random_id(),
+                keyboard=keyboard.get_keyboard(),
+                message="Пример клавиатуры",
+            )
 
 
 if __name__ == "__main__":
     load_dotenv()
     vk_token = os.getenv("VK_TOKEN")
 
-    vk_session = vk.VkApi(token=vk_token)
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+    main(vk_token)
