1939f4436fca5b2ef65eff714e65051cbb09f7b4
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-12 21:36:42 +0300

    Handle VK new question request

--- vk_bot.py
+++ vk_bot.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 import vk_api
 from dotenv import load_dotenv
@@ -7,26 +6,40 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
+from questions import get_random_quiz, QUIZ_FILEPATH, REDIS_CONN
 
-def main(vk_token: str):
-    vk_session = vk_api.VkApi(token=vk_token)
-    vk = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
 
+def set_keyboard():
     keyboard = VkKeyboard(one_time=True)
     keyboard.add_button("Новый вопрос", color=VkKeyboardColor.PRIMARY)
     keyboard.add_button("Сдаться", color=VkKeyboardColor.NEGATIVE)
     keyboard.add_line()
     keyboard.add_button("Мой счет", color=VkKeyboardColor.POSITIVE)
 
+    return keyboard.get_keyboard()
+
+
+def handle_new_question_request(event: VkLongPoll, vk: vk_api):
+    question, answer = get_random_quiz(QUIZ_FILEPATH)
+    REDIS_CONN.set(name=event.user_id, value=question)
+
+    vk.messages.send(
+        user_id=event.user_id,
+        message=question,
+        keyboard=set_keyboard(),
+        random_id=get_random_id(),
+    )
+
+
+def main(vk_token: str):
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            vk.messages.send(
-                peer_id=event.user_id,
-                random_id=get_random_id(),
-                keyboard=keyboard.get_keyboard(),
-                message="Пример клавиатуры",
-            )
+            if event.text == "Новый вопрос":
+                handle_new_question_request(event, vk)
 
 
 if __name__ == "__main__":
